5945efd3ebd93b49830bc42053c389bc29dc2ac5
emin <mustafazadeemin2000@gmail.com>
2023-01-13 23:17:03 +0400

    creating first user in db

---
+++ prisma/migrations/20230113191442_optional_field/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "users" ALTER COLUMN "confirmPassword" DROP NOT NULL;
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import * as argon from 'argon2';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { SignUpDto } from './dto';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma) {}
+  constructor(private prisma: PrismaService) {}
   async signup(dto: SignUpDto) {
     try {
       //^ hash password
       const hashedPassword = await argon.hash(dto.password);
 
       //^ save user in db
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          password: hashedPassword,
+          fullName: dto.fullName,
+        },
+      });
+
       //^ return token
 
       return { dto };
