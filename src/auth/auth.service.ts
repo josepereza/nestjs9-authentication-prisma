a9e25a126826079d25fadeb389b4bc3ffeaa599c
emin <mustafazadeemin2000@gmail.com>
2023-01-13 23:10:27 +0400

    connecting to the db, prisma module

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import * as argon from 'argon2';
 import { SignUpDto } from './dto';
 
 @Injectable()
 export class AuthService {
-  signup(dto: SignUpDto) {
-    return { dto };
+  constructor(private prisma) {}
+  async signup(dto: SignUpDto) {
+    try {
+      //^ hash password
+      const hashedPassword = await argon.hash(dto.password);
+
+      //^ save user in db
+      //^ return token
+
+      return { dto };
+    } catch (error) {
+      return error;
+    }
   }
 }
